2805539fc17ddf4afd9cd3ca12befdf36883f73d
DanCodes <dan@dancodes.online>
2019-06-17 08:30:00 +0200

    add ;

--- preload.js
+++ preload.js
@@ -4,5 +4,5 @@ window.onload = function() {
     document.getElementsByTagName('header')[0].style.left = '0';
     document.getElementsByTagName('header')[0].style.width = '100%';
     document.getElementsByTagName('header')[0].style.zIndex = '9999999999999999';
-    document.getElementsByClassName('application-main')[0].style.marginTop = '7%'
+    document.getElementsByClassName('application-main')[0].style.marginTop = '7%';
 }
