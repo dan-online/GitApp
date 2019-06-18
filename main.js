efc96c20749b379ac259457836c27e28221ec507
DanCodes <dan@dancodes.online>
2019-06-18 15:42:16 +0200

    download fix

--- main.js
+++ main.js
@@ -19,9 +19,7 @@ autoUpdater.on('error', (err) => {
   createWindow();
 })
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log_message = 'Updating ' + progressObj.percent + '%';
   sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
